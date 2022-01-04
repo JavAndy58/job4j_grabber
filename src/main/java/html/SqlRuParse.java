d819a8f6041979fb6a1e3a3a98b3ec0be9ea8a6b
JavAndy58 <digital.designs.russia@gmail.com>
2022-01-04 11:45:26 +0300

    2. Парсинг HTML страницы. [#260358 #246724]

--- src/main/java/html/SqlRuParse.java
+++ src/main/java/html/SqlRuParse.java
@@ -7,7 +7,7 @@ import org.jsoup.select.Elements;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers&start=20").get();
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
         Elements row = doc.select(".postslisttopic");
         for (Element td : row) {
             Element parent = td.parent();
