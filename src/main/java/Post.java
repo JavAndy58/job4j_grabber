2b07d40e27a625a2a9acd0c9f5c693e1991f38e4
JavAndy58 <digital.designs.russia@gmail.com>
2022-01-05 16:36:45 +0300

    2.2. Модель данных - Post. [#285211 #247040]

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -17,12 +17,12 @@ public class Post {
             return false;
         }
         Post post = (Post) o;
-        return Objects.equals(title, post.title) && Objects.equals(link, post.link) && Objects.equals(description, post.description) && Objects.equals(created, post.created);
+        return id == post.id && title.equals(post.title) && link.equals(post.link) && created.equals(post.created);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(title, link, description, created);
+        return Objects.hash(id, title, link, created);
     }
 
     @Override
