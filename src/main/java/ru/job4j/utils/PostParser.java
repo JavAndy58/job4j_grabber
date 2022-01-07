c2b28b4a0b3ee152f065ef73d4d9658d410e17c4
JavAndy58 <digital.designs.russia@gmail.com>
2022-01-07 11:10:06 +0300

    2.3. Загрузка деталей поста. [#285212 #247554]

--- src/main/java/ru/job4j/utils/PostParser.java
+++ src/main/java/ru/job4j/utils/PostParser.java
@@ -2,8 +2,6 @@ package ru.job4j.utils;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import ru.job4j.Post;
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -14,12 +12,8 @@ public class PostParser {
         Document doc = Jsoup.connect(link).get();
         post.setTitle(doc.select(".messageHeader").get(0).ownText());
         post.setLink(link);
-        Elements row = doc.select("#content-wrapper-forum");
-        for (Element element : row) {
-            Element parent = element.parent();
-            post.setDescription(parent.child(1).child(3).child(0).child(1).child(1).text());
-            post.setCreated(parseDate(parent.child(1).child(3).child(0).child(2).child(0).text()));
-        }
+        post.setDescription(doc.select(".msgBody").get(1).text());
+        post.setCreated(parseDate(doc.select(".msgFooter").get(0).text()));
         return post;
     }
 
