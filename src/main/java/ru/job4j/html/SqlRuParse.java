e082a10383c92eecbd240eb3e19a0039d0cfe6d9
JavAndy58 <digital.designs.russia@gmail.com>
2022-01-08 17:25:24 +0300

    2.4. SqlRuParse [#285213 #247816]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -9,8 +9,12 @@ import ru.job4j.grabber.Parse;
 import ru.job4j.utils.DateTimeParser;
 import ru.job4j.utils.PostParser;
 import ru.job4j.utils.SqlRuDateTimeParser;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class SqlRuParse implements Parse {
 
@@ -21,20 +25,25 @@ public class SqlRuParse implements Parse {
     }
 
     @Override
-    public List<Post> list(String link) throws Exception {
+    public List<Post> list(String link) {
         List<Post> posts = new ArrayList<>();
         for (int counter = 1; counter < 6; counter++) {
             Post postTemp;
-            String stringUrl = link + counter;
-            String strLink;
-            Document doc = Jsoup.connect(stringUrl).get();
-            Elements row = doc.select(".postslisttopic");
+            String stringUrl = String.format(link, counter);
+            String stringLink;
+            Document doc = null;
+            try {
+                doc = Jsoup.connect(stringUrl).get();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Elements row = Objects.requireNonNull(doc).select(".postslisttopic");
             for (Element td : row) {
                 Element parent = td.parent();
-                strLink = parent.child(1).child(0).attr("href");
-                postTemp = detail(strLink);
-                if ((postTemp.getTitle().contains("java") || postTemp.getTitle().contains("Java"))
-                    && (!postTemp.getTitle().contains("Javascript") || !postTemp.getTitle().contains("javascript"))) {
+                stringLink = parent.child(1).child(0).attr("href");
+                postTemp = detail(stringLink);
+                if (postTemp.getTitle().toLowerCase().contains("java")
+                        && !postTemp.getTitle().toLowerCase().contains("javascript")) {
                     posts.add(postTemp);
                 }
             }
@@ -43,10 +52,23 @@ public class SqlRuParse implements Parse {
     }
 
     @Override
-    public Post detail(String link) throws Exception {
-        Post postTemp;
-        PostParser postParser = new PostParser(dateTimeParser);
-        postTemp = postParser.parse(link);
+    public Post detail(String link) {
+        Post postTemp = new Post();
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(link).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String parseSub = (Objects.requireNonNull(doc).select(".msgFooter").get(0).text()).substring(0, 16);
+        postTemp.setTitle(doc.select(".messageHeader").get(0).ownText());
+        postTemp.setLink(link);
+        postTemp.setDescription(doc.select(".msgBody").get(1).text());
+        try {
+            postTemp.setCreated(dateTimeParser.parse(parseSub));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return postTemp;
     }
 }
