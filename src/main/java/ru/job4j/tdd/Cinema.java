e1e2f54840b2c116d75d84c9916a3827ede3e566
JavAndy58 <digital.designs.russia@gmail.com>
2022-01-20 18:08:15 +0300

    2. Что такое TDD? [#4918]

--- src/main/java/ru/job4j/tdd/Cinema.java
+++ src/main/java/ru/job4j/tdd/Cinema.java
@@ -1,6 +1,7 @@
 package ru.job4j.tdd;
 
 import java.util.Calendar;
+import java.util.List;
 import java.util.function.Predicate;
 
 public interface Cinema {
--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -1,12 +1,22 @@
 package ru.job4j.tdd;
 
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
-
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
 import java.util.function.Predicate;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CinemaTest {
 
+    @Ignore
     @Test
     public void whenBuy() {
         Account account = new AccountCinema();
@@ -17,6 +27,7 @@ public class CinemaTest {
         assertThat(ticket, is(new Ticket3D()));
     }
 
+    @Ignore
     @Test
     public void whenFind() {
         Cinema cinema = new Cinema3D();
@@ -25,12 +36,31 @@ public class CinemaTest {
         assertThat(sessions, is(Arrays.asList(new Session3D())));
     }
 
+    @Ignore
     @Test
-    public void whenAdd() {
-        Cinema cinema = new Cinema();
-        cinema.add(new Session3D());
-        cinema.add(new Session4D());
-        List<Session> sessions = cinema.find(session -> true);
-        assertThat(sessions, is(Arrays.asList(new Session3D(), new Session4D())));
+    public void whenRowNull() {
+        Cinema cinema = new Cinema3D();
+        int result = cinema.getRow(null);
+        assertThat(result, is(nullValue()));
+    }
+
+    @Ignore
+    @Test
+    public void whenColumnNull() {
+        Cinema cinema = new Cinema3D();
+        int result = cinema.getColumn(null);
+        assertThat(result, is(nullValue()));
+    }
+
+    @Ignore
+    @Test
+    public void whenTicketClone() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket1 = cinema.buy(account, 1, 1, date);
+        Ticket ticket2 = cinema.buy(account, 1, 1, date);
+        assertEquals(ticket1, is(ticket2));
     }
 }
