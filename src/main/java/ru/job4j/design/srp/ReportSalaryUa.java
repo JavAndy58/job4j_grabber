385a66013c590f343a5bd0f8a1b3f5b3d30e3e1c
JavAndy58 <digital.designs.russia@gmail.com>
2022-01-25 07:10:03 +0300

    1. Отчеты. [#850 #254905]

--- src/main/java/ru/job4j/design/srp/ReportSalaryUa.java
+++ src/main/java/ru/job4j/design/srp/ReportSalaryUa.java
@@ -5,9 +5,11 @@ import java.util.function.Predicate;
 public class ReportSalaryUa implements Report {
 
     private Store store;
+    private final double greatBritishPound;
 
     public ReportSalaryUa(Store store) {
         this.store = store;
+        greatBritishPound = 100;
     }
 
     @Override
@@ -18,7 +20,7 @@ public class ReportSalaryUa implements Report {
             text.append(employee.getName()).append(";")
                     .append(employee.getHired()).append(";")
                     .append(employee.getFired()).append(";")
-                    .append(employee.getSalary() / 100).append(";")
+                    .append(employee.getSalary() / greatBritishPound).append(";")
                     .append(System.lineSeparator());
         }
         return text.toString();
