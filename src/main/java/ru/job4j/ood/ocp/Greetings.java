78ce3cb513e5eb172a7b5a648df6643979554379
JavAndy58 <digital.designs.russia@gmail.com>
2022-01-26 14:01:41 +0300

    0. Принцип открытости закрытости [#4914 #255429]

--- src/main/java/ru/job4j/ood/ocp/Greetings.java
+++ src/main/java/ru/job4j/ood/ocp/Greetings.java
@@ -4,10 +4,13 @@ import java.util.Objects;
 
 public class Greetings {
 
+    private Year year;
     private String formality;
 
-    public Greetings(String formality) {
+
+    public Greetings(String formality, Year year) {
         this.formality = formality;
+        this.year = year;
     }
 
     public String greet() {
@@ -21,11 +24,11 @@ public class Greetings {
         } else {
             temp = "Hello.";
         }
-        return temp;
+        return temp + " " + year.yearning();
     }
 
     public static void main(String[] args) {
-        Greetings greetings = new Greetings("formal");
+        Greetings greetings = new Greetings("formal", new Year());
         System.out.printf("%s, Petr", greetings.greet());
     }
 }
---
+++ src/main/java/ru/job4j/ood/ocp/Year.java
@@ -0,0 +1,7 @@
+package ru.job4j.ood.ocp;
+
+public class Year {
+    public int yearning() {
+        return 2022;
+    }
+}
