e36e0fd35b7c1e798ff43819a7386607a48189ff
JavAndy58 <digital.designs.russia@gmail.com>
2022-01-27 14:45:05 +0300

    1. Поддержка форматом XML, JSON в генераторе отчетов. [#851 #256116]

--- src/test/java/ru/job4j/design/srp/ReportJSONTest.java
+++ src/test/java/ru/job4j/design/srp/ReportJSONTest.java
@@ -4,24 +4,21 @@ import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
 import org.junit.Test;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class ReportJSONTest {
 
     @Test
     public void whenJSONGenerated() {
         MemStore store = new MemStore();
-        Calendar now = Calendar.getInstance();
+        Calendar now = new GregorianCalendar(2022, 01, 27);
         Employee worker = new Employee("Ivan", now, now, 100);
         store.add(worker);
         Report engine = new ReportJSON(store);
-
-        StringBuilder expect = new StringBuilder().append("[{")
-                .append("\"").append("name").append("\"").append(":")
-                .append("\"").append(worker.getName()).append("\",")
-                .append("\"").append("hired").append("\"").append(":");
-
-
-
+        StringBuilder expect = new StringBuilder()
+                .append("[{\"name\":\"Ivan\",\"hired\":{\"year\":2022,\"month\":1,\"dayOfMonth\":27,\"hourOfDay\":0,"
+                        + "\"minute\":0,\"second\":0},\"fired\":{\"year\":2022,\"month\":1,\"dayOfMonth\":27,"
+                        + "\"hourOfDay\":0,\"minute\":0,\"second\":0},\"salary\":100.0}]");
         assertThat(engine.generate(em -> true), is(expect.toString()));
     }
 }
---
+++ src/test/java/ru/job4j/design/srp/ReportXMLTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.design.srp;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.function.Predicate;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.junit.Test;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class ReportXMLTest {
+
+    @Test
+    public void whenXMLGenerated() {
+        MemStore store = new MemStore();
+        Calendar now = new GregorianCalendar(2022, 01, 27);
+        Employee worker = new Employee("Ivan", now, now, 100);
+        store.add(worker);
+        Report engine = new ReportXML(store);
+        StringBuilder expect = new StringBuilder()
+                .append("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>").append("\n")
+                .append("<employees>").append("\n").append("    ")
+                .append("<employees>").append("\n").append("        ")
+                .append("<fired>2022-02-27T00:00:00+03:00</fired>").append("\n").append("        ")
+                .append("<hired>2022-02-27T00:00:00+03:00</hired>").append("\n").append("        ")
+                .append("<name>Ivan</name>").append("\n").append("        ")
+                .append("<salary>100.0</salary>").append("\n").append("    ")
+                .append("</employees>").append("\n")
+                .append("</employees>").append("\n");
+        assertThat(engine.generate(em -> true), is(expect.toString()));
+    }
+}
