fb206bc0a45fd5e053737b3d7378d3c876891b8e
JavAndy58 <digital.designs.russia@gmail.com>
2022-02-09 09:33:46 +0300

    update

---
+++ src/main/java/ru/job4j/ood/dip/A.java
@@ -0,0 +1,33 @@
+package ru.job4j.ood.dip;
+
+public class A {
+    void someMethod() {
+        B b = new B();
+        b.someMethodOfB();
+    }
+}
+class B {
+    void someMethodOfB() {
+        System.out.println("Hello world");
+    }
+}
+
+class BackEndDeveloper {
+    public void writeJava() {
+    }
+}
+
+class FrontEndDeveloper {
+    public void writeJavascript() {
+    }
+}
+
+class Project {
+    private BackEndDeveloper backEndDeveloper = new BackEndDeveloper();
+    private FrontEndDeveloper frontEndDeveloper = new FrontEndDeveloper();
+
+    public void implement() {
+        backEndDeveloper.writeJava();
+        frontEndDeveloper.writeJavascript();
+    }
+}
