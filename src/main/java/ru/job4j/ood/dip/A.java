0cfb1b332ff6f38b1cbd729eeee74cc40d23b82d
JavAndy58 <digital.designs.russia@gmail.com>
2022-02-12 11:35:22 +0300

    readme_1

--- src/main/java/ru/job4j/ood/dip/A.java
+++ src/main/java/ru/job4j/ood/dip/A.java
@@ -1,15 +1,25 @@
 package ru.job4j.ood.dip;
 
+import java.security.MessageDigest;
+
 public class A {
     void someMethod() {
         B b = new B();
         b.someMethodOfB();
     }
+
+    public static void main(String[] args) throws Exception {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] digest = md.digest("abracadabra".getBytes("UTF-8"));
+        for (byte b : digest) {
+            System.out.printf("%02x", b);
+        }
+    }
 }
 class B {
 
     void someMethodOfB() {
-        System.out.println("Hello world");
+//        System.out.println("Hello world");
     }
 }
 
